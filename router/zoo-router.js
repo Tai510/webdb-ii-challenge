32200dd76df3f0964f1fb4c580fd20cab94c56bd
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-04 15:03:03 -0700

    get post and put working on postman

--- router/zoo-router.js
+++ router/zoo-router.js
@@ -22,4 +22,55 @@ const knexConfig = {
   })
  })
 
+ router.get("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ message: "no such id exists" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  db('zoos', 'id')
+    .insert(req.body)
+    .then(ids => {
+      const [id] = ids; 
+      db('zoos')
+        .where({ id })
+        .first()
+        .then(zoo => {
+          res.status(200).json(zoo);
+        });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  db('zoos')
+  .where({id: req.params.id})
+  .update(req.body)
+  .then(count => {
+    if(count > 0) {
+      res.status(200).json({
+       message: `${count} record updated!`});
+    } else {
+      res.status(404).json({ message: 'Name not found'});
+    }
+  }).catch(error => {
+    res.status(500).json(error);
+    console.error(error)
+  })
+});
+
+
 module.exports = router;
